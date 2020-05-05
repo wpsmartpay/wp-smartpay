dcabc1514638118a02f864514e0511fff2bda1f0
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-05-05 15:51:28 +0600

    Added product folder

---
+++ includes/products/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SmartPay\Product;
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) exit;
+final class Product
+{
+    /**
+     * The single instance of this class
+     */
+    private static $instance = null;
+
+    /**
+     * Construct Product class.
+     *
+     * @since 0.1
+     * @access private
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * Main Product Instance.
+     *
+     * Ensures that only one instance of Product exists in memory at any one
+     * time. Also prevents needing to define globals all over the place.
+     *
+     * @since 0.1
+     * @return object|Product
+     * @access public
+     */
+    public static function instance()
+    {
+        if (!isset(self::$instance) && !(self::$instance instanceof Product)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+}
