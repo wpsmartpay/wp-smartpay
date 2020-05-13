3530ceb280fd6fba8ec15ce4e4d9573bf91e1e89
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-05-13 10:50:18 +0600

    Improved session functions

--- src/Session.php
+++ src/Session.php
@@ -6,12 +6,31 @@ namespace SmartPay;
 if (!defined('ABSPATH')) {
     exit;
 }
+
 final class Session
 {
     /**
      * The single instance of this class
      */
     private static $instance = null;
+
+    /**
+     * Holds our session data
+     *
+     * @var array
+     * @access private
+     * @since 1.5
+     */
+    private $session;
+
+    /**
+     * Session index prefix
+     *
+     * @var string
+     * @access private
+     * @since 2.3
+     */
+    private $session_key = 'SMARTPAY_SESSION';
 
     /**
      * Construct Session class.
@@ -21,6 +40,88 @@ final class Session
      */
     private function __construct()
     {
+        $this->init_session();
+    }
+
+    /**
+     * Set a session variable
+     *
+     * @since 1.5
+     *
+     * @param string $key Session key
+     * @param int|string|array $value Session variable
+     * @return mixed Session variable
+     */
+    public function set($key, $value)
+    {
+        $key = sanitize_key($key);
+
+        if (is_array($value)) {
+            $this->session[$key] = wp_json_encode($value);
+        } else {
+            $this->session[$key] = esc_attr($value);
+        }
+
+        $_SESSION[$this->session_key] = $this->session;
+
+        return $this->session[$key];
+    }
+
+    /**
+     * Retrieve a session variable
+     *
+     * @since 1.5
+     * @param string $key Session key
+     * @return mixed Session variable
+     */
+    public function get($key)
+    {
+        $key    = sanitize_key($key);
+        $return = false;
+
+        if (isset($this->session[$key]) && !empty($this->session[$key])) {
+
+            preg_match('/[oO]\s*:\s*\d+\s*:\s*"\s*(?!(?i)(stdClass))/', $this->session[$key], $matches);
+            if (!empty($matches)) {
+                $this->set($key, null);
+                return false;
+            }
+
+            if (is_numeric($this->session[$key])) {
+                $return = $this->session[$key];
+            } else {
+
+                $maybe_json = json_decode($this->session[$key]);
+
+                // Since json_last_error is PHP 5.3+, we have to rely on a `null` value for failing to parse JSON.
+                if (is_null($maybe_json)) {
+                    $is_serialized = is_serialized($this->session[$key]);
+                    if ($is_serialized) {
+                        $value = @unserialize($this->session[$key]);
+                        $this->set($key, (array) $value);
+                        $return = $value;
+                    } else {
+                        $return = $this->session[$key];
+                    }
+                } else {
+                    $return = json_decode($this->session[$key], true);
+                }
+            }
+        }
+
+        return $return;
+    }
+
+    // TODO: Implement unset
+
+    public function set_payment($payment_data)
+    {
+        return $this->set('smartpay_payment', $payment_data);
+    }
+
+    public function get_payment()
+    {
+        return $this->get('smartpay_payment');
     }
 
     /**
@@ -40,5 +141,18 @@ final class Session
         }
 
         return self::$instance;
+    }
+
+    public function init_session()
+    {
+        if (!session_id()) {
+            session_start();
+        }
+
+        if (is_multisite()) {
+            $this->session_key = 'SMARTPAY_SESSION_' . strtoupper(get_current_blog_id());
+        }
+
+        $this->session = isset($_SESSION[$this->session_key]) && is_array($_SESSION[$this->session_key]) ? $_SESSION[$this->session_key] : [];
     }
 }
