ea63d0be4630de5e9cb7cc81dd6dfca173c4f83d
Parvez Akther <parvez@themexpert.com>
2020-05-15 02:23:52 +0600

    Product page partial done

--- includes/views/shortcodes/product.php
+++ includes/views/shortcodes/product.php
@@ -9,46 +9,57 @@ $has_payment_error = false;
 ?>
 
 <div class="smartpay">
-    <h3><?php echo $product->name; ?></h3>
-    <p><?php echo smartpay_amount_format($product->sale_price); ?></p>
-    <p><?php echo $product->description; ?></p>
+	<div class="card">
+		<div class="bg-light border-bottom">
+			<img src="//static-2.gumroad.com/res/gumroad/6164410083782/asset_previews/1141ebca31596778ba2dd76532e2caa7/retina/responsive.png" class="card-img-top" alt="<?php echo $product->name; ?>">
+		</div>
+		<div class="card-body p-5">
+			<div class="row">
+				<div class="col-8">
+					<p class=""><?php echo smartpay_amount_format($product->base_price); ?></p>
+					<h2 class="card-title mt-0 mb-2"><?php echo $product->name; ?></h2>
+					<div class="card-text"><?php echo $product->description; ?></div>
+				</div>
+				<div class="col">
+					<form action="<?php echo $form_action; ?>" method="POST">
 
-    <!-- Modal content -->
-    <form action="<?php echo $form_action; ?>" method="POST">
+						<?php wp_nonce_field('smartpay_process_payment', 'smartpay_process_payment'); ?>
 
-        <?php wp_nonce_field('smartpay_process_payment', 'smartpay_process_payment'); ?>
+						<input type="hidden" name="smartpay_action" value="smartpay_process_payment">
+						<input type="hidden" name="smartpay_purchase_type" value="product_purchase">
+						<input type="hidden" name="smartpay_product_id" value="<?php echo $product->get_ID() ?>">
+						<!-- // TODO: Implement variations -->
+						<ul class="list-unstyled">
+							<?php if (count($gateways)) : ?>
 
-        <input type="hidden" name="smartpay_action" value="smartpay_process_payment">
-        <input type="hidden" name="smartpay_purchase_type" value="product_purchase">
-        <input type="hidden" name="smartpay_product_id" value="<?php echo $product->get_ID() ?>">
-        <!-- // TODO: Implement variations -->
-        <ul>
-            <?php if (count($gateways)) : ?>
+							<?php foreach ($gateways as $gateway_id => $gateway) : ?>
+							<li>
+								<?php echo '<label for="smartpay-gateway-' . esc_attr($gateway_id) . '">
+										<input type="radio" name="smartpay_gateway" id="smartpay-gateway-' . esc_attr($gateway_id) . '" value="' . esc_attr($gateway_id) . '"' . checked($gateway_id, $chosen_gateway, false) . '>';
+										echo esc_html($gateway['checkout_label']);
+										echo '</label>';
+										?>
+							</li>
+							<?php endforeach; ?>
 
-            <?php foreach ($gateways as $gateway_id => $gateway) : ?>
-            <li>
-                <?php echo '<label for="smartpay-gateway-' . esc_attr($gateway_id) . '">
-                        <input type="radio" name="smartpay_gateway" id="smartpay-gateway-' . esc_attr($gateway_id) . '" value="' . esc_attr($gateway_id) . '"' . checked($gateway_id, $chosen_gateway, false) . '>';
-                        echo esc_html($gateway['checkout_label']);
-                        echo '</label>';
-                        ?>
-            </li>
-            <?php endforeach; ?>
+							<?php else : ?>
+							<?php
+								$has_payment_error = true;
+								echo 'You must enable a payment gateway to proceed a payment.';
+								?>
+							<?php endif; ?>
+						</ul>
 
-            <?php else : ?>
-            <?php
-                $has_payment_error = true;
-                echo 'You must enable a payment gateway to proceed a payment.';
-                ?>
-            <?php endif; ?>
-        </ul>
+						<input type="text" name="smartpay_first_name" value="Al-Amin">
+						<input type="text" name="smartpay_last_name" value="Firdows">
+						<input type="text" name="smartpay_email" value="alaminfirdows@gmail.com">
+						<br>
 
-        <input type="text" name="smartpay_first_name" value="Al-Amin">
-        <input type="text" name="smartpay_last_name" value="Firdows">
-        <input type="text" name="smartpay_email" value="alaminfirdows@gmail.com">
-        <br>
-
-        <button type="submit" class="smartpay product-buy" <?php if ($has_payment_error) echo 'disabled'; ?>>
-            <?php echo $payment_button_text ?? 'Pay Now' ?></button>
-    </form>
-</div>
+						<button type="submit" class="btn btn-primary btn-block btn-lg" <?php if ($has_payment_error) echo 'disabled'; ?>>
+							<?php echo $payment_button_text ?? 'Pay Now' ?></button>
+						</form>
+				</div>
+			</div> <!-- row -->
+		</div> <!-- card-body -->
+	</div> <!-- card -->
+</div> <!-- .smartpay -->
