0b06c71254d7c0d511abccce05c584e9495c50ed
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-05-31 13:52:09 +0600

    Added Basic markup to payment details page

--- includes/views/admin/payments/details.php
+++ includes/views/admin/payments/details.php
@@ -1,3 +1,43 @@
-<?php
+<div class="wrap">
+    <h1 class="wp-heading-inline">Payment Details</h1>
+    <hr class="wp-header-end">
 
-echo 'payment details page';
+    <div id="poststuff">
+        <div id="post-body" class="metabox-holder columns-2">
+            <div id="postbox-container-1" class="postbox-container">
+                <div id="submitdiv" class="postbox">
+                    <h2 class="hndle"><span>Publish</span></h2>
+                    <div class="inside">
+                        <div class="submitbox" id="submitpost">
+                            <div id="major-publishing-actions">
+                                <div id="publishing-action">
+                                    <span class="spinner"></span>
+                                    <input name="original_publish" type="hidden" id="original_publish" value="Publish">
+                                    <input type="submit" name="publish" id="publish"
+                                        class="button button-primary button-large" value="Publish"> </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="postbox-container-2" class="postbox-container">
+                <div id="normal-sortables">
+                    <div id="smartpay-form-metabox-data" class="postbox ">
+                        <h2 class="hndle"><span>Payment Form Options</span></h2>
+                        <div class="inside">
+                        </div>
+                    </div>
+                    <div id="authordiv" class="postbox ">
+                        <h2 class="hndle"><span>Author</span></h2>
+                        <div class="inside">
+                        </div>
+                    </div>
+                </div>
+                <div id="advanced-sortables"></div>
+            </div>
+        </div><!-- /post-body -->
+        <br class="clear">
+    </div><!-- /poststuff -->
+
+</div>
