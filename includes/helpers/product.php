2efce763aef27bd3765f8a878ed6550da698535a
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-06-01 13:50:35 +0600

    Added function for product variation

--- includes/helpers/product.php
+++ includes/helpers/product.php
@@ -6,3 +6,10 @@ function smartpay_get_product($product_id)
 
     return SmartPay()->product->get_product($product_id);
 }
+
+function smartpay_get_product_variation($variation_id)
+{
+    if (!$variation_id) return;
+
+    return SmartPay()->product->get_product_variation($variation_id);
+}
