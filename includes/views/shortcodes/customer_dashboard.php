0647fa4fc662e20d7f2a2058e0003093bce7ef39
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-06-18 20:10:21 +0600

    Added update process for shortcode profile

--- includes/views/shortcodes/customer_dashboard.php
+++ includes/views/shortcodes/customer_dashboard.php
@@ -1,22 +1,22 @@
 <style>
-    .profile img {
-        height: 90px;
-        width: 90px;
-        box-shadow: 0 5px 15px rgba(0, 0, 0, 0.08);
-    }
+.profile img {
+    height: 90px;
+    width: 90px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.08);
+}
 
-    .profile h3 {
-        font-size: 20px;
-    }
+.profile h3 {
+    font-size: 20px;
+}
 
-    .product--header {
-        cursor: pointer;
-    }
+.product--header {
+    cursor: pointer;
+}
 
-    table td,
-    table th {
-        border: none;
-    }
+table td,
+table th {
+    border: none;
+}
 </style>
 
 <div class="smartpay">
@@ -47,42 +47,42 @@
                             <div class="tab-content">
                                 <div class="tab-pane fade show active" id="payments" role="tabpanel">
                                     <?php if (!is_array($customer->all_payments()) || !count($customer->all_payments())) : ?>
-                                        <div class="card">
-                                            <div class="card-body py-5">
-                                                <p class="text-info  m-0 text-center"><?php _e('You don\'t have any payment yet.', 'smartpay'); ?></p>
-                                            </div>
+                                    <div class="card">
+                                        <div class="card-body py-5">
+                                            <p class="text-info  m-0 text-center"><?php _e('You don\'t have any payment yet.', 'smartpay'); ?></p>
                                         </div>
+                                    </div>
                                     <?php else : ?>
-                                        <div class="table-responsive">
-                                            <table class="table table-hover">
-                                                <thead>
-                                                    <tr>
-                                                        <th scope="col"><?php _e('Order ID', 'smartpay'); ?></th>
-                                                        <th scope="col"><?php _e('Date', 'smartpay'); ?></th>
-                                                        <th scope="col"><?php _e('Item', 'smartpay'); ?>(s)</th>
-                                                        <th scope="col"><?php _e('Status', 'smartpay'); ?></th>
-                                                        <th scope="col"><?php _e('Amount', 'smartpay'); ?></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
+                                    <div class="table-responsive">
+                                        <table class="table table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col"><?php _e('Order ID', 'smartpay'); ?></th>
+                                                    <th scope="col"><?php _e('Date', 'smartpay'); ?></th>
+                                                    <th scope="col"><?php _e('Item', 'smartpay'); ?>(s)</th>
+                                                    <th scope="col"><?php _e('Status', 'smartpay'); ?></th>
+                                                    <th scope="col"><?php _e('Amount', 'smartpay'); ?></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
 
-                                                    <?php foreach ($customer->all_payments() as $index => $payment) : ?>
-                                                        <tr>
-                                                            <th scope="row"><?php echo '#' . $payment->ID; ?></th>
-                                                            <td><?php echo mysql2date('F j, Y', $payment->date); ?></td>
-                                                            <td><?php echo 'Items'; ?></td>
-                                                            <td class="<?php echo 'publish' == $payment->status ? 'text-success' : 'text-danger'; ?>">
-                                                                <?php echo $payment->status_nicename; ?></td>
-                                                            <td class="text-muted">
-                                                                <strong class="<?php echo 'publish' == $payment->status ? 'text-success' : 'text-danger'; ?>">
-                                                                    <?php echo smartpay_amount_format($payment->amount, $payment->currency); ?>
-                                                                </strong>
-                                                            </td>
-                                                        </tr>
-                                                    <?php endforeach; ?>
-                                                </tbody>
-                                            </table>
-                                        </div>
+                                                <?php foreach ($customer->all_payments() as $index => $payment) : ?>
+                                                <tr>
+                                                    <th scope="row"><?php echo '#' . $payment->ID; ?></th>
+                                                    <td><?php echo mysql2date('F j, Y', $payment->date); ?></td>
+                                                    <td><?php echo 'Items'; ?></td>
+                                                    <td class="<?php echo 'publish' == $payment->status ? 'text-success' : 'text-danger'; ?>">
+                                                        <?php echo $payment->status_nicename; ?></td>
+                                                    <td class="text-muted">
+                                                        <strong class="<?php echo 'publish' == $payment->status ? 'text-success' : 'text-danger'; ?>">
+                                                            <?php echo smartpay_amount_format($payment->amount, $payment->currency); ?>
+                                                        </strong>
+                                                    </td>
+                                                </tr>
+                                                <?php endforeach; ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
                                     <?php endif ?>
                                 </div>
 
@@ -95,59 +95,68 @@
                                     ?>
                                     <?php foreach (range(1, 2) as $i) : ?>
 
-                                        <div class="border mb-3 product">
-                                            <div class="p-3  product--header">
-                                                <div class="row" data-toggle="collapse" data-target="#collapse<?php echo $i; ?>">
-                                                    <div class="col-sm-2 product--image">
-                                                        <img src="<?php echo $product->image; ?>" class="border" alt="">
-                                                    </div>
-                                                    <div class="col-sm-7">
-                                                        <h5 class="mt-0"><?php echo $product->title; ?></h5>
-                                                        <p><strong>Variation:</strong> Pro</p>
-                                                    </div>
+                                    <div class="border mb-3 product">
+                                        <div class="p-3  product--header">
+                                            <div class="row" data-toggle="collapse" data-target="#collapse<?php echo $i; ?>">
+                                                <div class="col-sm-2 product--image">
+                                                    <img src="<?php echo $product->image; ?>" class="border" alt="">
+                                                </div>
+                                                <div class="col-sm-7">
+                                                    <h5 class="mt-0"><?php echo $product->title; ?></h5>
+                                                    <p><strong>Variation:</strong> Pro</p>
                                                 </div>
                                             </div>
-                                            <div class="p-3 bg-light collapse show" id="collapse<?php echo $i; ?>">
-                                                <p>Files</p>
-                                                <ul class="list-group">
-                                                    <?php foreach (range(1, 5) as $i) : ?>
-                                                        <li class="list-group-item p-2">
-                                                            <div class="d-flex align-items-center flex-wrap">
-                                                                <div>
-                                                                    <img src="<?php echo $product->image; ?>" style="height: 40px;" alt="">
-                                                                </div>
-                                                                <div class="ml-3">
-                                                                    <p class="m-0"><?php echo $product->title; ?></p>
-                                                                    <p class="text-muted m-0"><small>Size: 10kb</small></p>
-                                                                </div>
-                                                                <div class="ml-auto">
-                                                                    <button class="btn btn-sm btn-primary mr-1"><?php _e('Download', 'smartpay'); ?></button>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                            </div>
                                         </div>
+                                        <div class="p-3 bg-light collapse show" id="collapse<?php echo $i; ?>">
+                                            <p>Files</p>
+                                            <ul class="list-group">
+                                                <?php foreach (range(1, 5) as $i) : ?>
+                                                <li class="list-group-item p-2">
+                                                    <div class="d-flex align-items-center flex-wrap">
+                                                        <div>
+                                                            <img src="<?php echo $product->image; ?>" style="height: 40px;" alt="">
+                                                        </div>
+                                                        <div class="ml-3">
+                                                            <p class="m-0"><?php echo $product->title; ?></p>
+                                                            <p class="text-muted m-0"><small>Size: 10kb</small></p>
+                                                        </div>
+                                                        <div class="ml-auto">
+                                                            <button class="btn btn-sm btn-primary mr-1"><?php _e('Download', 'smartpay'); ?></button>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <?php endforeach; ?>
+                                            </ul>
+                                        </div>
+                                    </div>
                                     <?php endforeach; ?>
                                 </div>
 
                                 <div class="tab-pane fade" id="profile" role="tabpanel">
-                                    <form class="my-5">
+                                    <div class="alert alert-danger text-center">
+                                        You should check in on some of those fields below.
+                                    </div>
+                                    <?php
+                                    global $wp;
+                                    $current_url = home_url(add_query_arg(array(), $wp->request));
+                                    ?>
+                                    <form class="my-5" action="<?php echo $current_url; ?>" method="POST">
+                                        <?php wp_nonce_field('smartpay_process_profile_update', 'smartpay_process_profile_update'); ?>
+
                                         <div class="form-row mb-2">
                                             <div class="form-group col">
                                                 <label for="first_name"><?php _e('First Name', 'smartpay'); ?></label>
-                                                <input type="text" name="first_name" id="first_name" class="form-control" value="<?php echo $customer->first_name ?? '' ?>" placeholder="<?php _e('First Name', 'smartpay'); ?>">
+                                                <input type="text" name="first_name" id="first_name" class="form-control" value="<?php echo esc_attr($customer->first_name ?? '') ?>" placeholder="<?php _e('First Name', 'smartpay'); ?>">
                                             </div>
                                             <div class="form-group col">
                                                 <label for="last_name"><?php _e('Last Name', 'smartpay'); ?></label>
-                                                <input type="text" name="last_name" id="last_name" class="form-control" value="<?php echo $customer->last_name ?? '' ?>" placeholder="<?php _e('Last Name', 'smartpay'); ?>">
+                                                <input type="text" name="last_name" id="last_name" class="form-control" value="<?php echo esc_attr($customer->last_name ?? '') ?>" placeholder="<?php _e('Last Name', 'smartpay'); ?>">
                                             </div>
                                         </div>
 
                                         <div class="form-group mb-4">
                                             <label for="email"><?php _e('Email', 'smartpay'); ?></label>
-                                            <input type="email" name="email" id="email" class="form-control" value="<?php echo $customer->email ?? '' ?>" placeholder=" <?php _e('Email', 'smartpay'); ?>">
+                                            <input type="email" name="email" id="email" class="form-control" value="<?php echo esc_attr($customer->email ?? '') ?>" placeholder=" <?php _e('Email', 'smartpay'); ?>">
                                         </div>
 
                                         <div class="form-group mb-4">
@@ -158,12 +167,12 @@
                                         <div class="form-row mb-2">
                                             <div class="form-group col-6">
                                                 <label><?php _e('Password', 'smartpay'); ?></label>
-                                                <input type="password" class="form-control" placeholder="<?php _e('Password', 'smartpay'); ?>">
+                                                <input type="password" name="password" id="password" class="form-control" placeholder="<?php _e('Password', 'smartpay'); ?>">
                                                 <small class="form-text text-muted"><?php _e('If you don\'t want to change password, then ignore this fields.', 'smartpay'); ?></small>
                                             </div>
                                             <div class="form-group col-6">
                                                 <label><?php _e('Confirm Password', 'smartpay'); ?></label>
-                                                <input type="password" class="form-control" placeholder="<?php _e('Confirm Password', 'smartpay'); ?>">
+                                                <input type="password" name="password_confirm" id="password_confirm" class="form-control" placeholder="<?php _e('Confirm Password', 'smartpay'); ?>">
                                             </div>
                                         </div>
                                         <div class="d-flex justify-content-center mt-4">
--- src/Customers/Customer.php
+++ src/Customers/Customer.php
@@ -21,6 +21,8 @@ final class Customer
      */
     private function __construct()
     {
+        // Process shortcode profile update
+        add_action('init', [$this, 'process_shortcode_update_user_profile']);
     }
 
     /**
@@ -42,6 +44,47 @@ final class Customer
         return self::$instance;
     }
 
+    /**
+     * Update shortcode dashboard user profile.
+     *
+     * @since 0.0.1
+     * @access public
+     */
+    public function process_shortcode_update_user_profile()
+    {
+        // TODO: Add flush message
+
+        if (!isset($_POST['smartpay_process_profile_update']) || !wp_verify_nonce($_POST['smartpay_process_profile_update'], 'smartpay_process_profile_update')) {
+            return;
+        }
+
+        if (!is_user_logged_in() || get_current_user_id() <= 0) return;
+
+        extract(sanitize_post($_POST));
+
+        if (empty($first_name) || empty($last_name) || empty($email)) return;
+
+        if (isset($password) && (!isset($password_confirm) || $password !== $password_confirm)) 'Error.';
+
+        $user_data = wp_update_user(array(
+            'ID' => get_current_user_id(),
+            'display_name' => $first_name  . ' ' . $last_name,
+            'user_email' => $email,
+        ));
+
+        if (is_wp_error($user_data)) {
+            echo 'Error.';
+        } else {
+            echo 'User profile updated.';
+        }
+    }
+
+    /**
+     * Create customer database table.
+     *
+     * @since 0.0.1
+     * @access public
+     */
     public static function create_db_table()
     {
         return (new DB_Customer)->create_table();
