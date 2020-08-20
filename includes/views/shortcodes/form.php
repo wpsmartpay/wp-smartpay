1fefae3749198d4eed78754441f12ad274652590
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-08-20 13:23:38 +0600

    Reverse condition for shortcode

--- includes/views/shortcodes/form.php
+++ includes/views/shortcodes/form.php
@@ -8,7 +8,92 @@ $chosen_gateway = isset($_gateway) && smartpay_is_gateway_active($_gateway) ? $_
 $has_payment_error = false;
 ?>
 
-<?php if ('embedded' != $behavior) : ?>
+<?php if ('embedded' == $behavior) : ?>
+    <div class="smartpay">
+        <div class="smartpay-form-shortcode smartpay-payment">
+            <!-- Form details -->
+            <div class="card form">
+                <?php if ($form->image) : ?>
+                    <div class="bg-light form--image border-bottom">
+                        <img src="<?php echo $form->image; ?>" class="card-img-top">
+                    </div>
+                <?php endif; ?>
+
+                <div class="card-body p-5">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-7 mb-3">
+                            <?php if ($form->title) : ?>
+                                <h2 class="card-title form--title mt-0 mb-2"><?php echo $form->title; ?></h2>
+                            <?php endif; ?>
+
+                            <?php if ($form->description) : ?>
+                                <div class="card-text form--description">
+                                    <?php echo $form->description; ?>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+
+                        <div class="col-sm-12 col-md-5">
+                            <div class="form--amount-section">
+                                <div class="form-amounts mb-2">
+                                    <?php
+                                    $form_amounts = $form->get_amounts() ?? [];
+                                    $form_amount = reset($form_amounts) ?? 0;
+                                    ?>
+                                    <label class="form-amounts--label d-block m-0 mb-2"><?php _e('Select an amount', 'smartpay'); ?></label>
+                                    <ul class="list-group m-0">
+                                        <?php if ($form->has_multiple_amount()) : ?>
+                                            <!-- // Multiple amounts -->
+                                            <?php foreach ($form_amounts as $index => $amount) : ?>
+                                                <li class="list-group-item amount form--fixed-amount m-0 mb-3 py-4 <?php echo 0 == $index ? 'selected' : ''; ?>">
+                                                    <label for="_form_amount_<?php echo esc_attr($index); ?>" class="d-block m-0">
+                                                        <input class="d-none" type="radio" name="_form_amount" id="_form_amount_<?php echo esc_attr($index); ?>" value="<?php echo esc_attr($amount); ?>" <?php echo 0 == $index ? 'checked' : ''; ?>>
+                                                        <h5 class="m-0 amount--title"><?php echo smartpay_amount_format($amount); ?></h5>
+                                                    </label>
+                                                </li>
+
+                                            <?php endforeach; ?>
+
+                                            <!-- Form amount -->
+                                        <?php else : ?>
+                                            <li class="list-group-item amount m-0 mb-3 py-4 selected">
+                                                <label for="_form_amount" class="d-block m-0">
+                                                    <input class="d-none" type="radio" name="_form_amount" id="_form_amount" value="<?php echo esc_attr($form_amount); ?>" checked>
+                                                    <h5 class="m-0"><?php echo smartpay_amount_format($form_amount); ?></h5>
+                                                </label>
+                                            </li>
+                                        <?php endif; ?>
+                                    </ul>
+
+                                    <!-- // Allow custom payment -->
+                                    <?php
+                                    $form_suggested_amount = is_array($form_amounts) ? intval(array_sum($form_amounts) / count($form_amounts)) : $form_amount ?? 0;
+                                    ?>
+                                    <div class="form-group custom-amount-wrapper my-4 <?php echo !$form->allow_custom_amount ? 'd-none' : '' ?>">
+                                        <label for="smartpay_custom_amount" class="form-amounts--label d-block m-0 mb-2"><?php _e('Pay custom amount', 'smartpay'); ?></label>
+                                        <input type="text" class="form-control form--custom-amount amount" id="smartpay_custom_amount" name="smartpay_form_amount" value="<?php echo esc_attr($form_amount); ?>" placeholder="<?php echo esc_attr($form_suggested_amount); ?>">
+                                    </div>
+                                </div>
+
+                                <button type="button" class="btn btn-success btn-block btn-lg open-payment-form">
+                                    <?php echo _e('Pay', 'smartpay'); ?>
+                                </button>
+                            </div>
+                        </div> <!-- col -->
+                    </div> <!-- row -->
+                </div> <!-- card-body -->
+            </div>
+
+            <!-- Form Data -->
+            <input type="hidden" name="smartpay_payment_type" id="smartpay_payment_type" value="form_payment">
+            <input type="hidden" name="smartpay_form_id" id="smartpay_form_id" value="<?php echo $form->ID ?? 0; ?>">
+            <!-- /Form Data -->
+
+            <!-- Payment modal -->
+            <?php include "shared/payment-modal.php" ?>
+        </div>
+    </div>
+<?php else : ?>
     <div class="smartpay" style="background: transparent;">
         <div class="smartpay-product-shortcode">
             <div class="modal fade product-modal">
@@ -111,92 +196,6 @@ $has_payment_error = false;
             <button type="button" class="btn btn-success open-product-modal">
                 <?php echo _e($label ?: 'Pay now', 'smartpay'); ?>
             </button>
-        </div>
-    </div>
-
-<?php else : ?>
-    <div class="smartpay">
-        <div class="smartpay-form-shortcode smartpay-payment">
-            <!-- Form details -->
-            <div class="card form">
-                <?php if ($form->image) : ?>
-                    <div class="bg-light form--image border-bottom">
-                        <img src="<?php echo $form->image; ?>" class="card-img-top">
-                    </div>
-                <?php endif; ?>
-
-                <div class="card-body p-5">
-                    <div class="row">
-                        <div class="col-sm-12 col-md-7 mb-3">
-                            <?php if ($form->title) : ?>
-                                <h2 class="card-title form--title mt-0 mb-2"><?php echo $form->title; ?></h2>
-                            <?php endif; ?>
-
-                            <?php if ($form->description) : ?>
-                                <div class="card-text form--description">
-                                    <?php echo $form->description; ?>
-                                </div>
-                            <?php endif; ?>
-                        </div>
-
-                        <div class="col-sm-12 col-md-5">
-                            <div class="form--amount-section">
-                                <div class="form-amounts mb-2">
-                                    <?php
-                                    $form_amounts = $form->get_amounts() ?? [];
-                                    $form_amount = reset($form_amounts) ?? 0;
-                                    ?>
-                                    <label class="form-amounts--label d-block m-0 mb-2"><?php _e('Select an amount', 'smartpay'); ?></label>
-                                    <ul class="list-group m-0">
-                                        <?php if ($form->has_multiple_amount()) : ?>
-                                            <!-- // Multiple amounts -->
-                                            <?php foreach ($form_amounts as $index => $amount) : ?>
-                                                <li class="list-group-item amount form--fixed-amount m-0 mb-3 py-4 <?php echo 0 == $index ? 'selected' : ''; ?>">
-                                                    <label for="_form_amount_<?php echo esc_attr($index); ?>" class="d-block m-0">
-                                                        <input class="d-none" type="radio" name="_form_amount" id="_form_amount_<?php echo esc_attr($index); ?>" value="<?php echo esc_attr($amount); ?>" <?php echo 0 == $index ? 'checked' : ''; ?>>
-                                                        <h5 class="m-0 amount--title"><?php echo smartpay_amount_format($amount); ?></h5>
-                                                    </label>
-                                                </li>
-
-                                            <?php endforeach; ?>
-
-                                            <!-- Form amount -->
-                                        <?php else : ?>
-                                            <li class="list-group-item amount m-0 mb-3 py-4 selected">
-                                                <label for="_form_amount" class="d-block m-0">
-                                                    <input class="d-none" type="radio" name="_form_amount" id="_form_amount" value="<?php echo esc_attr($form_amount); ?>" checked>
-                                                    <h5 class="m-0"><?php echo smartpay_amount_format($form_amount); ?></h5>
-                                                </label>
-                                            </li>
-                                        <?php endif; ?>
-                                    </ul>
-
-                                    <!-- // Allow custom payment -->
-                                    <?php
-                                    $form_suggested_amount = is_array($form_amounts) ? intval(array_sum($form_amounts) / count($form_amounts)) : $form_amount ?? 0;
-                                    ?>
-                                    <div class="form-group custom-amount-wrapper my-4 <?php echo !$form->allow_custom_amount ? 'd-none' : '' ?>">
-                                        <label for="smartpay_custom_amount" class="form-amounts--label d-block m-0 mb-2"><?php _e('Pay custom amount', 'smartpay'); ?></label>
-                                        <input type="text" class="form-control form--custom-amount amount" id="smartpay_custom_amount" name="smartpay_form_amount" value="<?php echo esc_attr($form_amount); ?>" placeholder="<?php echo esc_attr($form_suggested_amount); ?>">
-                                    </div>
-                                </div>
-
-                                <button type="button" class="btn btn-success btn-block btn-lg open-payment-form">
-                                    <?php echo _e('Pay', 'smartpay'); ?>
-                                </button>
-                            </div>
-                        </div> <!-- col -->
-                    </div> <!-- row -->
-                </div> <!-- card-body -->
-            </div>
-
-            <!-- Form Data -->
-            <input type="hidden" name="smartpay_payment_type" id="smartpay_payment_type" value="form_payment">
-            <input type="hidden" name="smartpay_form_id" id="smartpay_form_id" value="<?php echo $form->ID ?? 0; ?>">
-            <!-- /Form Data -->
-
-            <!-- Payment modal -->
-            <?php include "shared/payment-modal.php" ?>
         </div>
     </div>
 <?php endif; ?>
--- includes/views/shortcodes/product.php
+++ includes/views/shortcodes/product.php
@@ -8,10 +8,89 @@ $_gateway = \sanitize_text_field($_REQUEST['gateway'] ?? '');
 
 $chosen_gateway = isset($_gateway) && smartpay_is_gateway_active($_gateway) ? $_gateway : smartpay_get_default_gateway();
 $has_payment_error = false;
-
+// var_dump($behavior);
+// exit;
 ?>
 
-<?php if ('embedded' != $behavior) : ?>
+<?php if ('embedded' == $behavior) : ?>
+    <div class="smartpay">
+        <div class="smartpay-product-shortcode smartpay-payment">
+            <!-- Product details -->
+            <div class="card product">
+                <?php if ($product->image) : ?>
+                    <div class="bg-light product--image border-bottom">
+                        <img src="<?php echo $product->image; ?>" class="card-img-top">
+                    </div>
+                <?php endif; ?>
+
+                <div class="card-body p-5">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-7 mb-3">
+                            <?php if ($product->title) : ?>
+                                <h2 class="card-title product--title mt-0 mb-2"><?php echo $product->title; ?></h2>
+                            <?php endif; ?>
+
+                            <?php if ($product->description) : ?>
+                                <div class="card-text product--description">
+                                    <?php echo $product->description; ?>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+
+                        <div class="col-sm-12 col-md-5">
+                            <div class="product--price-section">
+                                <div class="product-variations mb-2">
+                                    <ul class="list-group m-0">
+                                        <?php if ($product->has_variations()) : ?>
+                                            <!-- // Variation prices -->
+                                            <?php foreach ($product->variations as $index => $variation) : ?>
+                                                <li class="list-group-item variation price m-0 mb-3 py-4 <?php echo 0 == $index ? 'selected' : ''; ?>">
+                                                    <label for="smartpay_product_variation_id_<?php echo esc_attr($variation['id']); ?>" class="d-block m-0">
+                                                        <input class="d-none" type="radio" name="smartpay_product_variation_id" id="smartpay_product_variation_id_<?php echo esc_attr($variation['id']); ?>" value="<?php echo esc_attr($variation['id']); ?>" <?php echo 0 == $index ? 'checked' : ''; ?>>
+                                                        <span class="price--amount"><?php echo smartpay_amount_format(($product_price + $variation['additional_amount'])); ?></span>
+                                                        <strong class="price--title"><?php echo esc_html(ucfirst($variation['name'])); ?></strong>
+                                                        <?php if ($variation['description'] ?? false) : ?>
+                                                            <p class="variation--description m-0">
+                                                                <?php echo esc_attr($variation['description']); ?>
+                                                            </p>
+                                                        <?php endif; ?>
+                                                    </label>
+                                                </li>
+
+                                            <?php endforeach; ?>
+
+                                            <!-- Product price -->
+                                        <?php else : ?>
+                                            <li class="list-group-item price m-0 my-2 py-4 selected">
+                                                <label class="d-block m-0">
+                                                    <span class="price--amount"><?php echo smartpay_amount_format($product_price); ?></span>
+                                                    <h5 class="m-0 mt-3 price--title">Item Price</h5>
+                                                </label>
+                                            </li>
+                                        <?php endif; ?>
+                                    </ul>
+                                </div>
+
+                                <button type="button" class="btn btn-success btn-block btn-lg open-payment-form">
+                                    <?php echo _e('Pay', 'smartpay'); ?>
+                                </button>
+                            </div>
+                        </div> <!-- col -->
+                    </div> <!-- row -->
+                </div> <!-- card-body -->
+            </div>
+
+            <!-- Form Data -->
+            <input type="hidden" name="smartpay_payment_type" id="smartpay_payment_type" value="product_purchase">
+            <input type="hidden" name="smartpay_product_id" id="smartpay_product_id" value="<?php echo $product->ID ?? 0; ?>">
+            <!-- /Form Data -->
+
+            <!-- Payment modal -->
+            <?php include "shared/payment-modal.php" ?>
+        </div>
+    </div>
+
+<?php else : ?>
     <div class="smartpay" style="background: transparent;">
         <div class="smartpay-product-shortcode">
             <div class="modal fade product-modal">
@@ -106,84 +185,6 @@ $has_payment_error = false;
             <button type="button" class="btn btn-success open-product-modal">
                 <?php echo _e($label ?: 'Buy now', 'smartpay'); ?>
             </button>
-        </div>
-    </div>
-
-<?php else : ?>
-    <div class="smartpay">
-        <div class="smartpay-product-shortcode smartpay-payment">
-            <!-- Product details -->
-            <div class="card product">
-                <?php if ($product->image) : ?>
-                    <div class="bg-light product--image border-bottom">
-                        <img src="<?php echo $product->image; ?>" class="card-img-top">
-                    </div>
-                <?php endif; ?>
-
-                <div class="card-body p-5">
-                    <div class="row">
-                        <div class="col-sm-12 col-md-7 mb-3">
-                            <?php if ($product->title) : ?>
-                                <h2 class="card-title product--title mt-0 mb-2"><?php echo $product->title; ?></h2>
-                            <?php endif; ?>
-
-                            <?php if ($product->description) : ?>
-                                <div class="card-text product--description">
-                                    <?php echo $product->description; ?>
-                                </div>
-                            <?php endif; ?>
-                        </div>
-
-                        <div class="col-sm-12 col-md-5">
-                            <div class="product--price-section">
-                                <div class="product-variations mb-2">
-                                    <ul class="list-group m-0">
-                                        <?php if ($product->has_variations()) : ?>
-                                            <!-- // Variation prices -->
-                                            <?php foreach ($product->variations as $index => $variation) : ?>
-                                                <li class="list-group-item variation price m-0 mb-3 py-4 <?php echo 0 == $index ? 'selected' : ''; ?>">
-                                                    <label for="smartpay_product_variation_id_<?php echo esc_attr($variation['id']); ?>" class="d-block m-0">
-                                                        <input class="d-none" type="radio" name="smartpay_product_variation_id" id="smartpay_product_variation_id_<?php echo esc_attr($variation['id']); ?>" value="<?php echo esc_attr($variation['id']); ?>" <?php echo 0 == $index ? 'checked' : ''; ?>>
-                                                        <span class="price--amount"><?php echo smartpay_amount_format(($product_price + $variation['additional_amount'])); ?></span>
-                                                        <strong class="price--title"><?php echo esc_html(ucfirst($variation['name'])); ?></strong>
-                                                        <?php if ($variation['description'] ?? false) : ?>
-                                                            <p class="variation--description m-0">
-                                                                <?php echo esc_attr($variation['description']); ?>
-                                                            </p>
-                                                        <?php endif; ?>
-                                                    </label>
-                                                </li>
-
-                                            <?php endforeach; ?>
-
-                                            <!-- Product price -->
-                                        <?php else : ?>
-                                            <li class="list-group-item price m-0 my-2 py-4 selected">
-                                                <label class="d-block m-0">
-                                                    <span class="price--amount"><?php echo smartpay_amount_format($product_price); ?></span>
-                                                    <h5 class="m-0 mt-3 price--title">Item Price</h5>
-                                                </label>
-                                            </li>
-                                        <?php endif; ?>
-                                    </ul>
-                                </div>
-
-                                <button type="button" class="btn btn-success btn-block btn-lg open-payment-form">
-                                    <?php echo _e('Pay', 'smartpay'); ?>
-                                </button>
-                            </div>
-                        </div> <!-- col -->
-                    </div> <!-- row -->
-                </div> <!-- card-body -->
-            </div>
-
-            <!-- Form Data -->
-            <input type="hidden" name="smartpay_payment_type" id="smartpay_payment_type" value="product_purchase">
-            <input type="hidden" name="smartpay_product_id" id="smartpay_product_id" value="<?php echo $product->ID ?? 0; ?>">
-            <!-- /Form Data -->
-
-            <!-- Payment modal -->
-            <?php include "shared/payment-modal.php" ?>
         </div>
     </div>
 <?php endif; ?>
