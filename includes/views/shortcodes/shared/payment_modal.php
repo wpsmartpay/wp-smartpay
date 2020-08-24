ddfeb5eaedbd6956c9e42cd899b03b1595dba54a
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-08-24 10:56:51 +0600

    Updated modal design

--- includes/views/shortcodes/shared/payment_modal.php
+++ includes/views/shortcodes/shared/payment_modal.php
@@ -7,26 +7,28 @@ $customer = is_user_logged_in() ? new SmartPay_Customer(get_current_user_id(), t
 
 <div class="modal fade payment-modal" data-backdrop="static" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-md" role="document">
-        <div class="modal-content align-content-between">
-            <div class="d-flex justify-content-between text-center p-4 mt-4">
-                <div class="modal-button">
-                    <button class="btn back-to-first-step"><i data-feather="arrow-left" width="21" height="21"></i></button>
-                </div>
-                <div class="d-flex flex-column align-content-between">
-                    <h4 class="payment-modal--small-title m-0 mb-3">
+        <div class="modal-content">
+            <div class="modal-header mx-auto">
+                <button class="btn back-to-first-step">
+                    <i data-feather="arrow-left" width="21" height="21"></i>
+                </button>
+
+                <div class="d-flex flex-column justify-content-center modal-title">
+                    <h4 class="payment-modal--small-title m-0">
                         <?php echo __('Make a Payment', 'smartpay'); ?>
                     </h4>
-                    <h2 class="payment-modal--title m-0 mb-3">
+                    <h2 class="payment-modal--title m-0">
                         <?php echo $product->title ?? $form->title ?? 'Product/Form'; ?>
                     </h2>
                 </div>
-                <div class="modal-button">
-                    <button class="btn modal-close"><i data-feather="x" width="21" height="21"></i></button>
-                </div>
+
+                <button class="btn modal-close">
+                    <i data-feather="x" width="21" height="21"></i>
+                </button>
             </div>
 
-            <div class="modal-body text-center mb-4 step-1">
-                <div class="col-11 pb-4 align-self-center">
+            <div class="modal-body p-0 text-center step-1">
+                <div class="align-self-center">
                     <form action="<?php echo $form_action; ?>" method="POST">
                         <?php wp_nonce_field('smartpay_process_payment', 'smartpay_process_payment'); ?>
 
@@ -34,24 +36,27 @@ $customer = is_user_logged_in() ? new SmartPay_Customer(get_current_user_id(), t
                             <!-- // If only one gateway activated -->
                             <?php if (count($gateways) == 1) : ?>
                             <?php $gateways_index = array_keys($gateways); ?>
-                            <!-- <p class="payment-gateway--label single-gateway"><?php //echo sprintf(__('Payment method - ', 'smartpay') . ' <strong>%s</strong>', esc_html(reset($gateways)['checkout_label'])); 
-                                                                                        ?></p> -->
+                            <!-- <p class="payment-gateway--label single-gateway"> -->
+                            <?php //echo sprintf(__('Payment method - ', 'smartpay') . ' <strong>%s</strong>', esc_html(reset($gateways)['checkout_label'])); 
+                                ?>
+                            <!-- </p> -->
                             <input class="d-none" type="radio" name="smartpay_gateway" id="smartpay_gateway" value="<?php echo esc_html(reset($gateways_index)); ?>" checked>
 
                             <!-- // If it has multiple payment gateway -->
                             <?php elseif (count($gateways) > 1) : ?>
-                            <p class="payment-gateway--label"><?php echo _e('Select a gateway', 'smartpay'); ?></p>
+                            <p class="payment-gateway--label"><?php echo _e('Select a payment method', 'smartpay'); ?></p>
 
-                            <ul class="gateways list-unstyled list-group-horizontal-sm list-group m-0 my-3 justify-content-center">
+                            <div class="gateways m-0 justify-content-center d-flex">
                                 <?php foreach ($gateways as $gateway_id => $gateway) : ?>
-                                <li class="gateway list-group-item p-0 m-0 px-3 py-1">
+                                <div class="gateway">
+                                    <input type="radio" class="d-none" name="smartpay_gateway" id="<?php echo 'smartpay_gateway_' . esc_attr($gateway_id); ?>" value="<?php echo esc_attr($gateway_id) ?>" <?php echo checked($gateway_id, $chosen_gateway, false); ?>>
                                     <label for="<?php echo 'smartpay_gateway_' . esc_attr($gateway_id); ?>" class="gateway--label">
-                                        <input type="radio" class="mr-2" name="smartpay_gateway" id="<?php echo 'smartpay_gateway_' . esc_attr($gateway_id); ?>" value="<?php echo esc_attr($gateway_id) ?>" <?php echo checked($gateway_id, $chosen_gateway, false); ?>>
-                                        <?php echo esc_html($gateway['checkout_label']); ?>
+                                        <img src="<?php echo SMARTPAY_PLUGIN_ASSETS . '/img//' . $gateway_id . '.png'; ?>" alt="">
+                                        <!-- <?php echo esc_html($gateway['checkout_label']); ?> -->
                                     </label>
-                                </li>
+                                </div>
                                 <?php endforeach; ?>
-                            </ul>
+                            </div>
                             <?php else : ?>
 
                             <?php
@@ -64,7 +69,7 @@ $customer = is_user_logged_in() ? new SmartPay_Customer(get_current_user_id(), t
                         <div class="payment-modal--errors" style="display: none"></div>
 
                         <div class="payment-modal--user-info mt-3">
-                            <div class="row">
+                            <div class="form-row">
                                 <div class="col-sm-6 form-group">
                                     <input type="text" placeholder="First name" class="form-control" name="smartpay_first_name" id="smartpay_first_name" value="<?php echo $customer->first_name ?? ''; ?>" autocomplete="first_name" required>
                                 </div>
@@ -72,10 +77,8 @@ $customer = is_user_logged_in() ? new SmartPay_Customer(get_current_user_id(), t
                                     <input type="text" placeholder="Last name" class="form-control" name="smartpay_last_name" id="smartpay_last_name" value="<?php echo $customer->last_name ?? ''; ?>" autocomplete="last_name" required>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col form-group">
-                                    <input type="email" placeholder="Email address" class="form-control" name="smartpay_email" id="smartpay_email" value="<?php echo $customer->email ?? ''; ?>" autocomplete="email" required>
-                                </div>
+                            <div class="form-group">
+                                <input type="email" placeholder="Email address" class="form-control" name="smartpay_email" id="smartpay_email" value="<?php echo $customer->email ?? ''; ?>" autocomplete="email" required>
                             </div>
 
                             <button type="button" class="btn btn-success btn-block btn-lg smartpay-pay-now" <?php if ($has_payment_error) echo 'disabled'; ?>>
@@ -91,8 +94,9 @@ $customer = is_user_logged_in() ? new SmartPay_Customer(get_current_user_id(), t
                 </div>
 
             </div>
-            <div class="modal-body text-center mb-4 step-2">
-                <div class="col-11 pb-4 align-self-center">
+
+            <div class="modal-body p-0 text-center step-2">
+                <div class="align-self-center">
                     <div class="mb-5">
                         <div class="alert alert-warning py-3">
                             <p class="m-0"><?php _e('Don\'t close this window before completing payment!', 'smartpay'); ?></p>
