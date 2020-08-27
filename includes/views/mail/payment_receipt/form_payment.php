9cca2b1645bab4ae9fa30404aebe50929e7512ef
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-08-27 17:02:49 +0600

    Added paid amount to form payment receipt

--- includes/views/mail/payment_receipt/form_payment.php
+++ includes/views/mail/payment_receipt/form_payment.php
@@ -1,4 +1,5 @@
 <?php $customer_name = $payment->customer['first_name'] . ' ' . $payment->customer['last_name']; ?>
 
 <p><?php echo __('Dear', 'smartpay') . ' ' . $customer_name . ','; ?></p>
+<?php echo __('Paid amount: ', 'smartpay') . ' ' . $payment->amount; ?>
 <p><?php _e('Thank you for your payment.', 'smartpay'); ?></p>
