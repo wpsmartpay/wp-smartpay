c53661a0b641f762fede8f647e290a4d150300d1
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-11-09 14:40:16 +0600

    Added form model

---
+++ app/Models/Form.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SmartPay\Models;
+
+use SmartPay\Framework\Database\Eloquent\Model;
+
+class Form extends Model
+{
+    protected $table = 'smartpay_forms';
+
+    protected $fillable = [];
+}
