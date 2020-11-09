20987d4ddae63b7ff2b1dbaca64752e2276654e1
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2020-11-09 18:36:08 +0600

    Added view route for coupon

--- app/Http/Controllers/Rest/CouponController.php
+++ app/Http/Controllers/Rest/CouponController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace SmartPay\Http\Controllers\Rest;
+
 use SmartPay\Http\Controllers\RestController;
 
 use SmartPay\Framework\Http\Request;
@@ -25,7 +26,7 @@ class CouponController extends RestController
 
     public function store(\WP_REST_Request $request)
     {
-        $data = \json_decode($request->get_body(),true);
+        $data = \json_decode($request->get_body(), true);
         $uid = get_current_user_id();
         $coupon = new Coupon();
         $coupon->title = $data['title'];
@@ -38,6 +39,18 @@ class CouponController extends RestController
         $coupon->save();
 
         $response = new \WP_REST_Response($coupon, 200);
+        return $response;
+    }
+
+    public function view(\WP_REST_Request $request)
+    {
+        $id = $request['id'];
+        $coupon = Coupon::find($id);
+        if (isset($coupon)) {
+            $response = new \WP_REST_Response($coupon, 200);
+        } else {
+            $response = new \WP_REST_Response($coupon, 400);
+        }
         return $response;
     }
 }
--- resources/js/pages/coupon/edit.js
+++ resources/js/pages/coupon/edit.js
@@ -1,3 +1,217 @@
+import { __ } from '@wordpress/i18n'
+import {
+    Container,
+    Form,
+    Tabs,
+    Tab,
+    Row,
+    Col,
+    Button,
+    Alert,
+} from 'react-bootstrap'
+import { useReducer, useEffect, useState } from '@wordpress/element'
+import { useParams } from 'react-router-dom'
+
+const initialState = {
+    title: '',
+    description: '',
+    discounttype: 'fixed',
+    amount: '',
+    expirydate: '',
+}
+
+const reducer = (state, action) => {
+    if (action.type == 'reset') {
+        return initialState
+    }
+
+    const result = { ...state }
+    result[action.type] = action.value
+    return result
+}
+
 export const EditCoupon = () => {
-    return <p>Coupon Edit</p>
+    let { couponId } = useParams()
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`${smartpay.restUrl}/v1/coupons/${couponId}`, {
+            method: 'GET',
+            headers: {
+                'X-WP-Nonce': smartpay.apiNonce,
+            },
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                setData({ ...data })
+            })
+    }, [couponId])
+    const [state, dispatch] = useReducer(reducer, data)
+
+    console.log(data)
+    console.log(state)
+
+    const changeHandler = (event) => {
+        dispatch({ type: event.target.name, value: event.target.value })
+    }
+
+    const couponUpdateHandler = (event) => {
+        event.preventDefault()
+
+        // fetch(`${resturl}/v1/coupons`, {
+        //     method: 'POST',
+        //     headers: {
+        //         'X-WP-Nonce': nonce,
+        //     },
+        //     body: JSON.stringify(state),
+        // }).then((response) => {
+        //     if (response.status == 200) {
+        //         let $alert = document.getElementById('coupon-alert')
+        //         $alert.classList.remove('d-none')
+        //     }
+        // })
+
+        // dispatch({ type: 'reset' })
+    }
+
+    return (
+        <>
+            <div className="text-black bg-white border-bottom d-fixed">
+                <Container>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h2 className="text-black">
+                            {__('SmartPay', 'smartpay')}
+                        </h2>
+                        <div className="ml-auto">
+                            <Button
+                                type="button"
+                                className="btn btn-primary btn-sm text-decoration-none"
+                                onClick={couponUpdateHandler}
+                            >
+                                {__('Update', 'smartpay')}
+                            </Button>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+            <Container>
+                <Row className="justify-content-center">
+                    <Col xs={9}>
+                        <Alert
+                            id="coupon-alert"
+                            className="mt-5 d-none"
+                            variant="success"
+                        >
+                            Coupon Added Successfully
+                        </Alert>
+                    </Col>
+                </Row>
+            </Container>
+
+            <div className="py-5">
+                <Container>
+                    <Row className="justify-content-center">
+                        <Col xs={9}>
+                            <Form>
+                                <Form.Group controlId="couponForm.title">
+                                    <Form.Control
+                                        name="title"
+                                        value={state.title}
+                                        onChange={changeHandler}
+                                        type="text"
+                                        placeholder="Enter coupon code here"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="couponForm.description">
+                                    <Form.Control
+                                        name="description"
+                                        value={state.description}
+                                        onChange={changeHandler}
+                                        as="textarea"
+                                        rows={3}
+                                        placeholder="Coupon description"
+                                    />
+                                </Form.Group>
+                                <div className="py-2">
+                                    <Tabs
+                                        className="mb-3"
+                                        fill
+                                        defaultActiveKey="home"
+                                    >
+                                        <Tab
+                                            tabClassName="text-decoration-none"
+                                            eventKey="home"
+                                            title="Home"
+                                        >
+                                            <Row>
+                                                <Col>
+                                                    <Form.Group controlId="couponForm.discountType">
+                                                        <Form.Label className="mb-2 d-inline-block">
+                                                            Discount type
+                                                        </Form.Label>
+                                                        <Form.Control
+                                                            name="discounttype"
+                                                            as="select"
+                                                            value={
+                                                                state.discounttype
+                                                            }
+                                                            onChange={
+                                                                changeHandler
+                                                            }
+                                                        >
+                                                            <option value="fixed">
+                                                                Fixed Amount
+                                                            </option>
+                                                            <option value="percent">
+                                                                Percent
+                                                            </option>
+                                                        </Form.Control>
+                                                    </Form.Group>
+                                                </Col>
+                                                <Col>
+                                                    <Form.Group controlId="couponForm.amount">
+                                                        <Form.Label className="mb-2 d-inline-block">
+                                                            Coupon amount
+                                                        </Form.Label>
+                                                        <Form.Control
+                                                            name="amount"
+                                                            value={state.amount}
+                                                            onChange={
+                                                                changeHandler
+                                                            }
+                                                            type="text"
+                                                            placeholder="0"
+                                                        />
+                                                    </Form.Group>
+                                                </Col>
+                                            </Row>
+                                            <Form.Group controlId="couponForm.expiryDate">
+                                                <Form.Label className="mb-2 d-inline-block">
+                                                    Coupon expiry date
+                                                </Form.Label>
+                                                <Form.Control
+                                                    name="expirydate"
+                                                    type="date"
+                                                    value={state.expirydate}
+                                                    onChange={changeHandler}
+                                                />
+                                            </Form.Group>
+                                        </Tab>
+                                        <Tab
+                                            tabClassName="text-decoration-none"
+                                            eventKey="usage-restriction"
+                                            title="Usage Restriction"
+                                        >
+                                            <p>Upgrade to pro</p>
+                                        </Tab>
+                                    </Tabs>
+                                </div>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </>
+    )
 }
