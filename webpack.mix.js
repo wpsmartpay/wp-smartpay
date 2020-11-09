f41dfd3d849521edb41e27a88ea014163c95ccb5
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-11-09 19:39:49 +0600

    Removed block editor from mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,6 +8,5 @@ mix.webpackConfig({
 })
     .js('resources/js/admin.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/block-editor/block-editor.scss', 'public/css')
     .sass('resources/sass/admin.scss', 'public/css')
     .sourceMaps(false)
