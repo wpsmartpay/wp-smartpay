b0fe3d6f7a339a873ff0eb50b7c2f2f8c13e02ee
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-11-10 01:59:35 +0600

    Deleted admin pages

--- resources/views/admin/form/create.php
+++
@@ -1,6 +0,0 @@
-<div id="smartpay" style="display:none;" class="smartpay">
-</div>
-
-<div id="smartpay-form-block-editor" class="smartpay-form-block-editor block-editor">
-    Loading Editor...
-</div>
--- resources/views/admin/product/shared/navbar.php
+++
@@ -1,7 +0,0 @@
-<div class="wrap d-none">
-    <h2></h2>
-</div>
-
-<div class="d-flex align-items-center justify-content-between px-3">
-    <h2 class="text-black"><?php _e($title ?: '', 'smartpay'); ?></h2>
-</div>
