53d645a2c44cafa5190d61b6e167ffde320b12c1
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-11-13 12:28:09 +0600

    Build assets

--- public/block-editor/blocks/index.asset.php
+++ public/block-editor/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '2018cdcda70fc7541e460ebff7583066');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'fe6bd3f1f638d985b6abcc95caf4c59c');
