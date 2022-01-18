c1d93a2fd3dc2a022d925b7579f07216b3dea427
Atiqur Rahman <ar.diu.cse@gmail.com>
2022-01-18 15:30:42 +0600

    feat: 🎸 added new user create after payment insertion

---
+++ app/Modules/Customer/CreateUser.php
@@ -0,0 +1,44 @@
+<?php
+namespace SmartPay\Modules\Customer;
+
+use SmartPay\Models\Payment;
+
+class CreateUser {
+
+    public function create_user(Payment $payment)
+    {
+        if (empty($username))
+        {
+            $username = $payment->email;
+        }
+        $email = sanitize_email($payment->email);
+        if (!username_exists($username) && !email_exists($email))
+        {
+            // Handle password creation.
+            $password_generated = false;
+            if ( empty( $password ) ) {
+                $password           = wp_generate_password();
+                $password_generated = true;
+            }
+
+            $new_customer_data = array(
+                'user_login' => $username,
+                'user_pass'  => $password,
+                'user_email' => $email,
+                'role'       => 'Customer',
+            );
+
+            try {
+                $user = wp_insert_user( $new_customer_data );
+                echo $user;
+                if ($user){
+                    //send notification
+                    wp_new_user_notification($user, null, 'both');
+                }
+            }catch (\Exception $e){
+                smartpay_debug_log(sprintf(__('SmartPay: User could not create, due to %s', 'smartpay'), $e->getMessage()));
+            }
+            return true;
+        }
+    }
+}
