3d7ff6aa10d537b0f09cf75cde656dd91ffdb429
Atiqur Rahman <ar.diu.cse@gmail.com>
2022-03-22 11:31:01 +0600

    fix: 🐛 dash issue on form subscription amounts

--- resources/views/shortcodes/shared/form_details.php
+++ resources/views/shortcodes/shared/form_details.php
@@ -21,7 +21,7 @@
                                     // FIXME: Move to pro plugin 
                                     ?>
                                     <?php if (\SmartPay\Models\Payment::BILLING_TYPE_SUBSCRIPTION === $billingType) : ?>
-                                        <label class="custom-control-label m-0 ml-1 amount--title" for="_form_amount_<?php echo $amount['key']; ?>"><?php echo $amount['label']; ?> - <?php echo smartpay_amount_format($amount['amount']); ?> / <?php echo  $amount['billing_period']; ?></label>
+                                        <label class="custom-control-label m-0 ml-1 amount--title" for="_form_amount_<?php echo $amount['key']; ?>"><?php echo $amount['label'] ? $amount['label'].'-' : ''; ?> <?php echo smartpay_amount_format($amount['amount']); ?> / <?php echo  $amount['billing_period']; ?></label>
                                     <?php else : ?>
                                         <label class="custom-control-label m-0 ml-1 amount--title" for="_form_amount_<?php echo $amount['key']; ?>"><?php echo $amount['label'] ? $amount['label'].'-' : ''; ?> <?php echo smartpay_amount_format($amount['amount']); ?></label>
                                     <?php endif; ?>
