f634010a891df20a3738ba391779bfb93b2d9cda
Atiqur Rahman <atiq.misterrahman@gmail.com>
2023-07-31 18:05:21 +0600

    fix: use createRoot instead of legacy render

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,7 +1,7 @@
 import domReady from '@wordpress/dom-ready'
 import { render } from '@wordpress/element'
 import { HashRouter, Route, Routes, Link } from 'react-router-dom'
-
+import { createRoot } from 'react-dom'
 import { Dashboard } from './pages/dashboard'
 
 // Product
@@ -38,106 +38,106 @@ import {EditInvoice} from "./pages/invoice/edit";
 export const smartPayRouteHooks = createHooks()
 window.smartPayRouteHooks = smartPayRouteHooks
 
-domReady(function () {
-    const SmartPay = () => {
-        return (
-            <div>
-                <HashRouter>
-                    <Routes>
-                        {/* Dashboard */}
-                        <Route exact path="/" element={<Dashboard />} />
+const SmartPay = () => {
+    return (
+        <div>
+            <HashRouter>
+                <Routes>
+                    {/* Dashboard */}
+                    <Route exact path="/" element={<Dashboard />} />
 
-                        {/* Product */}
-                        <Route
-                            exact
-                            path="/products"
-                            element={<ProductList />}
-                        />
-                        <Route
-                            exact
-                            path="/products/create"
-                            element={<CreateProduct />}
-                        />
-                        <Route
-                            exact
-                            path="/products/:productId/edit"
-                            element={<EditProduct />}
-                        />
+                    {/* Product */}
+                    <Route
+                        exact
+                        path="/products"
+                        element={<ProductList />}
+                    />
+                    <Route
+                        exact
+                        path="/products/create"
+                        element={<CreateProduct />}
+                    />
+                    <Route
+                        exact
+                        path="/products/:productId/edit"
+                        element={<EditProduct />}
+                    />
 
-                        {/* Customer */}
-                        <Route
-                            exact
-                            path="/customers"
-                            element={<CustomerList />}
-                        />
+                    {/* Customer */}
+                    <Route
+                        exact
+                        path="/customers"
+                        element={<CustomerList />}
+                    />
 
-                        {/* Invoices */}
-                        <Route
-                            exact
-                            path="/invoices"
-                            element={<Invoices />}
-                        />
+                    {/* Invoices */}
+                    <Route
+                        exact
+                        path="/invoices"
+                        element={<Invoices />}
+                    />
 
-                        <Route
-                            exact
-                            path="/invoices/create"
-                            element={<CreateInvoice />}
-                        />
+                    <Route
+                        exact
+                        path="/invoices/create"
+                        element={<CreateInvoice />}
+                    />
 
-                        <Route
-                            exact
-                            path="/invoices/:invoiceId/edit"
-                            element={<EditInvoice />}
-                        />
+                    <Route
+                        exact
+                        path="/invoices/:invoiceId/edit"
+                        element={<EditInvoice />}
+                    />
 
-                        <Route
-                            exact
-                            path="/customers/:customerId/"
-                            element={<ShowCustomer />}
-                        />
+                    <Route
+                        exact
+                        path="/customers/:customerId/"
+                        element={<ShowCustomer />}
+                    />
 
-                        {/* Coupon */}
-                        <Route exact path="/coupons" element={<CouponList />} />
-                        <Route
-                            exact
-                            path="/coupons/create"
-                            element={<CreateCoupon />}
-                        />
-                        <Route
-                            exact
-                            path="/coupons/:couponId/edit"
-                            element={<EditCoupon />}
-                        />
+                    {/* Coupon */}
+                    <Route exact path="/coupons" element={<CouponList />} />
+                    <Route
+                        exact
+                        path="/coupons/create"
+                        element={<CreateCoupon />}
+                    />
+                    <Route
+                        exact
+                        path="/coupons/:couponId/edit"
+                        element={<EditCoupon />}
+                    />
 
-                        {/* Payment */}
-                        <Route
-                            exact
-                            path="/payments"
-                            element={<PaymentList />}
-                        />
-                        <Route
-                            exact
-                            path="/payments/create"
-                            element={<CreatePayment />}
-                        />
-                        <Route
-                            exact
-                            path="/payments/:paymentId/edit"
-                            element={<EditPayment />}
-                        />
+                    {/* Payment */}
+                    <Route
+                        exact
+                        path="/payments"
+                        element={<PaymentList />}
+                    />
+                    <Route
+                        exact
+                        path="/payments/create"
+                        element={<CreatePayment />}
+                    />
+                    <Route
+                        exact
+                        path="/payments/:paymentId/edit"
+                        element={<EditPayment />}
+                    />
 
-                        <Route element={<NotFound />} />
-                    </Routes>
-                </HashRouter>
+                    <Route element={<NotFound />} />
+                </Routes>
+            </HashRouter>
 
-                {/* load the smartpay-pro routes */}
-                {window.smartPayRouteHooks.applyFilters(
-                    'smartPayAdminRoute',
-                    [],
-                )}
-            </div>
-        )
-    }
+            {/* load the smartpay-pro routes */}
+            {window.smartPayRouteHooks.applyFilters(
+                'smartPayAdminRoute',
+                [],
+            )}
+        </div>
+    )
+}
 
-    render(<SmartPay />, document.getElementById('smartpay'))
-})
+const container = document.getElementById('smartpay');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<SmartPay />);
