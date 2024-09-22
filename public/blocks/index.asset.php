5e9d0fb1104d1362ab3cdde468a253c63fd8ff85
aushamim <au.shamim0@gmail.com>
2024-09-22 14:52:01 +0600

    build: new build files

--- public/blocks/index.asset.php
+++ public/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react'), 'version' => 'a706a925ed17a46450ab');
+<?php return array('dependencies' => array('react'), 'version' => 'f5c18f3bcca1c2aaaac6');
