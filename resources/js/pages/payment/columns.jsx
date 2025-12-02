871155167d89a92041a80e619933d395c477159f
RoyHridoy <royhridoy1428@gmail.com>
2025-12-02 13:26:41 +0600

    feat: added product link and form id

--- resources/js/pages/payment/columns.jsx
+++ resources/js/pages/payment/columns.jsx
@@ -2,38 +2,61 @@ import { StatusBadge } from '@/components/status-badges'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { __ } from '@wordpress/i18n'
-import { Box, Eye, FilePenLine, Trash2 } from 'lucide-react'
+import { Box, Eye, FilePenLine, LinkIcon, Trash2 } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 export const createColumns = (deletePayment) => [
     {
-        accessorKey: 'id',
-        header: __('ID', 'smartpay'),
+		accessorKey: 'id',
+		header: __('ID', 'smartpay'),
 		enableSorting: false,
-    },
-    {
-        accessorKey: 'email',
-        header: __('Customer', 'smartpay'),
+	},
+	{
+		accessorKey: 'email',
+		header: __('Customer', 'smartpay'),
 		enableSorting: false,
-    },
-    {
-        accessorKey: 'type',
-        header: () => <div className="text-center">{ __('Type', 'smartpay') }</div>,
+	},
+	{
+		accessorKey: 'type',
+		header: () => <div className="text-center">{ __('Item & Type', 'smartpay') }</div>,
 		enableSorting: false,
 		cell: ({ row }) => {
 			const type = row.getValue('type');
+			const payment = row.original
+			const productId = payment?.data?.product_id
+			const formId = payment?.data?.form_id
+
 			if (type === 'Product Purchase') {
-				return <div className='flex justify-center items-center'><Badge variant="secondary" className="bg-slate-100 min-w-20"><Box className='size-3'/> Product</Badge></div>
+				return (
+					<div className='flex justify-center gap-2 items-center'>
+						<Badge variant="secondary" className="bg-slate-100 min-w-20"><Box className='size-3'/> Product</Badge>
+						<Badge variant="default" className="bg-slate-100 min-w-8">
+							<Link
+								to={`/products/${productId}/edit`}
+								className="text-slate-800! font-bold flex items-center justify-center hover:underline"
+							>
+								<LinkIcon className='size-3.5'/>
+							</Link>
+						</Badge>
+					</div>
+				)
 			} else if(type === 'Form Payment') {
-				return <div className='flex justify-center items-center'><Badge variant="secondary" className="bg-slate-100 min-w-20"><FilePenLine className='size-3'/> Form</Badge></div>
+				return (
+					<div className='flex justify-center gap-2 items-center'>
+						<Badge variant="secondary" className="bg-slate-100 min-w-20"><FilePenLine className='size-3'/> Form</Badge>
+						<Badge variant="default" className="bg-slate-100 min-w-8">
+							<span className='text-slate-700'>{`#${formId}`}</span>
+						</Badge>
+					</div>
+				)
 			}
 		}
-    },
-    {
-        accessorKey: 'date',
+	},
+	{
+		accessorKey: 'date',
 		header: () => <div className="text-center">{ __('Date & Time', 'smartpay') }</div>,
 		enableSorting: false,
-        cell: ({ row }) => {
+		cell: ({ row }) => {
 			const dateStr = row.original.completed_at || row.original.created_at;
 			const date = new Date(dateStr)
 			return (<div className='text-center'>
@@ -45,46 +68,46 @@ export const createColumns = (deletePayment) => [
 					minute: 'numeric',
 				})}
 			</div>)
-        },
-    },
-    {
-        accessorKey: 'status',
+		},
+	},
+	{
+		accessorKey: 'status',
 		header: () => <div className="text-center">{ __('Status', 'smartpay') }</div>,
 		enableSorting: false,
-        cell: ({ row }) => {
-            const status = row.getValue('status')
-            return (
+		cell: ({ row }) => {
+			const status = row.getValue('status')
+			return (
 				<div className='flex items-center justify-center'>
 					<StatusBadge status={status}/>
 				</div>
-            )
-        },
-    },
+			)
+		},
+	},
 	{
-        accessorKey: 'amount',
-        header: __('Amount', 'smartpay'),
-        cell: ({ row }) => {
-            const amount = parseFloat( row.getValue('amount') || 0 )
+		accessorKey: 'amount',
+		header: __('Amount', 'smartpay'),
+		cell: ({ row }) => {
+			const amount = parseFloat( row.getValue('amount') || 0 )
 			const formatted = new Intl.NumberFormat('en-US', {
 				style: 'currency',
 				currency: 'USD'
 			}).format(amount)
-            return (
+			return (
                 <div className='text-right pr-2'>
 					{formatted}
-                </div>
-            )
-        },
-    },
-    {
-        id: 'actions',
-        header: () => <div className="text-center">{ __('Actions', 'smartpay') }</div>,
-        cell: ({ row }) => {
-            const payment = row.original
+				</div>
+			)
+		},
+	},
+	{
+		id: 'actions',
+		header: () => <div className="text-center">{ __('Actions', 'smartpay') }</div>,
+		cell: ({ row }) => {
+			const payment = row.original
 
-            return (
-                <div className="flex items-center justify-end gap-2">
-                    <Link to={`/payments/${payment.id}/edit`}>
+			return (
+				<div className="flex items-center justify-end gap-2">
+					<Link to={`/payments/${payment.id}/edit`}>
 						<Button
 							variant="outline"
 							size="icon"
@@ -93,7 +116,7 @@ export const createColumns = (deletePayment) => [
 							>
 							<Eye className="w-4 h-4 text-gray-700" />
 						</Button>
-                    </Link>
+					</Link>
 					<Button
 						variant="outline"
 						size="icon"
@@ -103,8 +126,8 @@ export const createColumns = (deletePayment) => [
 					>
 						<Trash2 className="w-4 h-4 text-red-600" />
 					</Button>
-                </div>
-            )
-        },
-    },
+				</div>
+			)
+		},
+	},
 ]
